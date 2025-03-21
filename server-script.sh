097af97c74e877dfd3dc11b1fef4620f43fe662f
nikhilk1234 <104249202+nikhilk1234@users.noreply.github.com>
2025-03-21 19:16:39 +0530

    Create server-script.sh

---
+++ server-script.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+sudo yum install git -y
+sudo yum install docker -y
+sudo systemctl start docker
+
+if [ -d "addressbook" ]
+then 
+  echo "repo is already cloned"
+  cd addressbook/
+  git pull origin master 
+else
+    git clone https://github.com/nikhilk1234/addressbook.git
+    cd addressbook/
+fi
+sudo docker build -t image /home/ec2-user/addressbook
